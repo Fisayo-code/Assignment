42acc654c694e41dfc586dc8eef1492f6c29dba0
fisayo-code <obarofisayo7@gmail.com>
2025-09-15 16:52:23 +0100

    Javascript

---
+++ index.js
@@ -0,0 +1,50 @@
+prompt ('What is your name');
+alert ('successful');
+console.log ('Thank you');
+
+
+let name = 'Joy';
+name = 'Fisayo';
+age = 30 ;
+age =  33 ;
+age = age + 20;
+console.log (age);
+
+let address = "plot 23";
+name = 55;
+console.log (typeof address);
+console.log (typeof name);
+ 
+let isaprove = true;
+let isdecline = false;
+console.log (typeof isdecline);
+
+//addition operator
+
+let sum = 15 + 5;
+console.log ('Add:'+ sum)
+
+//subration operator
+
+let different = 15 - 5;
+console.log ('minu:' + different);
+
+//multiplecation operator
+
+let product = 15 * 5;
+console.log ('multiply:'+ product)
+
+//divition operator
+
+let quotient = 15 / 5
+console.log ('Divied:' + quotient)
+
+//concatenate
+
+let fristname = "John";
+let lastname = "Moon";
+console.log (fristname + " " + lastname);
+ 
+
+
+
